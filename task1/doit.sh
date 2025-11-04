f79481f99670b480dc7fade009a09724c03da30d
archit-ku <architkumar2705@gmail.com>
2025-11-04 00:34:48 +0000

    complete lab 1

---
+++ task1/doit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# cleanup
+rm -rf obj_dir
+rm -f counter.vcd
+
+verilator -Wall --cc --trace counter.sv --exe counter_tb.cpp
+
+make -j -C obj_dir/ -f Vcounter.mk Vcounter
+
+obj_dir/Vcounter
+
+~/Documents/iac/lab0-devtools/tools/attach_usb.sh
---
+++ task2/doit.sh
@@ -0,0 +1,12 @@
+~/Documents/iac/lab0-devtools/tools/attach_usb.sh
+#!/bin/sh
+
+# cleanup
+rm -rf obj_dir
+rm -f counter.vcd
+
+verilator -Wall --cc --trace counter.sv --exe counter_tb.cpp
+
+make -j -C obj_dir/ -f Vcounter.mk Vcounter
+
+obj_dir/Vcounter
